ff6d76fb52e889d6156a043bf70a823d23cfe0bc
Jamie Cotter <jcotter1998@hotmail.com>
2021-10-05 13:08:40 +0100

    Modified edge server to have more realistic resources

--- Services/NetworkTopologyServices/NetworkTopologyServices.cpp
+++ Services/NetworkTopologyServices/NetworkTopologyServices.cpp
@@ -120,7 +120,7 @@ float NetworkTopologyServices::getBandwidth(int source, int destination, Network
 
 NetworkTopology NetworkTopologyServices::generateNetwork() {
     ComputationNode cloudNodeA(4, 15, 128, 1000, cloud);
-    EdgeNode edgeNode(1, 10, 8, 250, node_type::edge, make_pair(1, 1));
+    EdgeNode edgeNode(4, 10, 8, 250, node_type::edge, make_pair(1, 1));
     MobileNode mobileNode(4, 4, 8, 16, mobile, make_pair(2, 2));
     ComputationNode cloudNodeB(4, 15, 128, 1000, cloud);
     ComputationNode cloudNodeC(4, 15, 128, 1000, cloud);
